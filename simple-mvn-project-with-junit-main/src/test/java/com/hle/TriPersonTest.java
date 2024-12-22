cfbd880ef9f4ef4c33c05a3b849360866599ddfd
Nouhaila Maarouf <nouhailamaarouf98@gmail.com>
2024-12-22 21:07:14 +0100

    test unitaire

--- simple-mvn-project-with-junit-main/src/test/java/com/hle/TriPersonTest.java
+++ simple-mvn-project-with-junit-main/src/test/java/com/hle/TriPersonTest.java
@@ -8,26 +8,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
 public class TriPersonTest {
-     @Test
-        public void testSortPerson() {
-                List<Person> people = new ArrayList<>();
-                people.add(Person.builder().firstName("Hala").familyName(" Jalal").build());
-                people.add(Person.builder().firstName("Marwan").familyName("Badr").build());
-                people.add(Person.builder().firstName("Hala").familyName("Chadi").build());
-                people.add(Person.builder().firstName("Bilal").familyName("Chadi").build());
+    @Test
+    public void testSortPerson() {
+            List<Person> people = new ArrayList<>();
+            people.add(Person.builder().firstName("Hamid").familyName("Jamila").build());
+            people.add(Person.builder().firstName("Martin").familyName("Bob").build());
+            people.add(Person.builder().firstName("Hamid").familyName("Charles").build());
+            people.add(Person.builder().firstName("Bernard").familyName("Charles").build());
 
-                // Tri de la liste de personnes
-                Collections.sort(people);
+            // Tri de la liste de personnes
+            Collections.sort(people);
 
-                assertThat(people.get(0))
-                                .isEqualTo(Person.builder().firstName("Marwan").familyName("Badr").build());
-                assertThat(people.get(1))
-                                .isEqualTo(Person.builder().firstName("Bilal").familyName("Chadi").build());
-                assertThat(people.get(2))
-                                .isEqualTo(Person.builder().firstName("Hala").familyName("Chadi").build());
-                assertThat(people.get(3))
-                                .isEqualTo(Person.builder().firstName("Hala").familyName("Jalal").build());
+            assertThat(people.get(0))
+                            .isEqualTo(Person.builder().firstName("Martin").familyName("Bob").build());
+            assertThat(people.get(1))
+                            .isEqualTo(Person.builder().firstName("Bernard").familyName("Charles").build());
+            assertThat(people.get(2))
+                            .isEqualTo(Person.builder().firstName("Hamid").familyName("Charles").build());
+            assertThat(people.get(3))
+                            .isEqualTo(Person.builder().firstName("Hamid").familyName("Jamila").build());
 
-        }
+    }
+
 
 }
